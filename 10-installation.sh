a7f99cb329227525d1169b2f46caf4bd2b717b6d
Harish <hramine@bgsu.edu>
2026-01-29 16:11:28 -0500

    actions

--- 10-installation.sh
+++ 10-installation.sh
@@ -2,9 +2,9 @@
 
 USERID= $(id -u)
 
-if [ USERID -ne 0 ]; then
-echo "please run this script with root user access"
-exit 1 
+if [ $USERID -ne 0 ]; then
+    echo "please run this script with root user access"
+    exit 1
 fi
 
 echo "Installing Nginx"
