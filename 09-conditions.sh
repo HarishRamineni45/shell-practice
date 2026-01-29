d432a9d28fea26d9ab63317523a407f430023cbf
Harish <hramine@bgsu.edu>
2026-01-29 15:52:40 -0500

    actions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -5,6 +5,7 @@ NUMBER=$1
 
 if [ $NUMBER -gt 20 ]; then 
      echo "Given Number : $NUMBER  is greater than 25"
+     elif  [ $NUMBER -eq 20 ]; then 
      else
      echo "Given Number : $NUMBER  is less than 25"
 fi
