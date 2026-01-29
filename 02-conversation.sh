0d7f3bb9227e106ff2a69755b98ca06acdc0b8bc
Harish <hramine@bgsu.edu>
2026-01-29 13:12:35 -0500

    actions

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "suresh :: hello ramesh how are you ?"
+echo "Ramesh:: I'm fine and how are you doing?"
+echo " Suresh :: doing fine.what are you learning Ramesh?"
+echo " Ramesh :: I am learning shell.what about you?"
