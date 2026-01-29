eb4e75536d96add1b2f602845d63f5f9439272cf
Harish <hramine@bgsu.edu>
2026-01-29 13:23:59 -0500

    actions

--- 03-variables.sh
+++ 03-variables.sh
@@ -7,5 +7,5 @@ PERSON2=Ramesh
 
 echo "$PERSON1 :: hello $PERSON2 how are you ?"
 echo "$PERSON2:: I'm fine and how are you doing?"
-echo " $PERSON1 :: doing fine.what are you learning Ramesh?"
+echo " $PERSON1 :: doing fine.what are you learning $PERSON2?"
 echo " $PERSON2 :: I am learning shell.what about you?"
