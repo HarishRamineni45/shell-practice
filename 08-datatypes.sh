637ccc44015caa46f050bd6e934ed284f54465d0
Harish <hramine@bgsu.edu>
2026-01-29 15:27:53 -0500

    actions

--- 08-datatypes.sh
+++ 08-datatypes.sh
@@ -7,3 +7,13 @@ SUM=$(($NUM1+$NUM2))
 
 echo "sum is $SUM"
 
+
+#Array
+
+FRUITS= ("Apple "banana" pomo")
+
+echo "fruits are :  ${FRUITS $[@]}"
+echo "First fruit is:  ${FRUITS $[0]}"
+echo "Second fruit is:  ${FRUITS $[1]}"
+echo "Third fruit is :  ${FRUITS $[2]}"
+
