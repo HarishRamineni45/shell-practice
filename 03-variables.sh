008ab73399d154ee6c1a0d77670caf6120416293
Harish <hramine@bgsu.edu>
2026-01-29 13:21:53 -0500

    actions

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=Suresh
+PERSON2=Ramesh
+
+
+
+echo "$PERSON1 :: hello $PERSON2 how are you ?"
+echo "$PERSON2:: I'm fine and how are you doing?"
+echo " $PERSON1 :: doing fine.what are you learning Ramesh?"
+echo " $PERSON2 :: I am learning shell.what about you?"
