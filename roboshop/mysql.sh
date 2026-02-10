5d1b673f0eabd1b7966889ab9caf5798fb9e6945
Harish <hramine@bgsu.edu>
2026-02-10 10:56:27 -0500

    hey

--- roboshop/mysql.sh
+++ roboshop/mysql.sh
@@ -27,14 +27,14 @@ VALIDATE(){
 }
 
 
-dnf install mysql-server -y
+dnf install mysql-server -y &>>$LOGS_FILE
 VALIDATE $? "Installing Mysql Server"
 
-systemctl enable mysqld
+systemctl enable mysqld &>>$LOGS_FILE
 VALIDATE $? "Enabling mysql server"
 
-systemctl start mysqld  
+systemctl start mysqld   &>>$LOGS_FILE
 VALIDATE $? "Start Mysql Server"
 
-mysql_secure_installation --set-root-pass RoboShop@1
+mysql_secure_installation --set-root-pass RoboShop@1 &>>$LOGS_FILE
 VALIDATE $? "setup root password"
